b8b2ef27becc98fa76f9a7cd6e98a09f52120bf4
Sandy Strong <sandy.strong@dreamhost.com>
2012-10-05 17:06:24 -0700

    added poll app

---
+++ mysite/polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Poll(models.Model):
+
+    def __unicode__(self):
+            return self.question
+
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+
+    def __unicode__(self):
+            return self.choice_text
+
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField()
