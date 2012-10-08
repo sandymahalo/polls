b3c057f9ba526056d83aa5508515a308a9700c93
Sandy Strong <sandy.strong@dreamhost.com>
2012-10-08 16:44:55 -0700

    tweaked gunicorn script

--- gunicorn.sh
+++ gunicorn.sh
@@ -6,6 +6,5 @@ NUM_WORKERS=3
 USER=deploy
 GROUP=deploy
 cd /var/www/polls/mysite
-source ../bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec ../bin/gunicorn_django -w $NUM_WORKERS \--user=$USER --group=$GROUP --log-level=debug \--log-file=$LOGFILE 2>>$LOGFILE
+exec /usr/local/bin/gunicorn_django -w $NUM_WORKERS \--user=$USER --group=$GROUP --log-level=debug \--log-file=$LOGFILE 2>>$LOGFILE
