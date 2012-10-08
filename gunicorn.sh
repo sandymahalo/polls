d426b9afede40e6fac7b1d2f5a01f2be0c527a99
Sandy Strong <sandy.strong@dreamhost.com>
2012-10-08 16:43:01 -0700

    fixed gunicorn file

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -eLOGFILE=/var/log/gunicorn/polls.log
+set -e
+LOGFILE=/var/log/gunicorn/polls.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=3
 USER=deploy
 GROUP=deploy
-cd /var/www/polls
+cd /var/www/polls/mysite
 source ../bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
 exec ../bin/gunicorn_django -w $NUM_WORKERS \--user=$USER --group=$GROUP --log-level=debug \--log-file=$LOGFILE 2>>$LOGFILE
