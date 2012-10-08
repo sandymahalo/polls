1e3bff797d7b8843687d0575d69b22d95400e1c3
Sandy Strong <sandy.strong@dreamhost.com>
2012-10-08 16:40:49 -0700

    added gunicorn file, modified fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import *
 
 # Hosts you wish to deploy to
-env.hosts = ['sandy@localhost:22']
+env.hosts = ['deploy@198.61.213.160:22']
 
 def pushpull():
     local('git push') # run command on local env
