65dbc33b0b82c8651f64c611c64c55a1dd483400
Sandy Strong <sandy.strong@dreamhost.com>
2012-10-16 12:17:12 -0700

    added restart fnc to fabfile

--- fabfile.py
+++ fabfile.py
@@ -6,3 +6,11 @@ env.hosts = ['deploy@198.61.213.160:22']
 def pushpull():
     local('git push') # run command on local env
     run('cd /var/www/polls/; git pull') #run command on remote env
+
+def restartservices():
+    # remotely, restart uWSGI
+    run('sudo service mysite-uwsgi restart')
+
+    # remotely, reload NGINX
+    run('sudo nginx -s reload')
+
